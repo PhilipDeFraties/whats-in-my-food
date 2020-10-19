469ec520ed0c04c67069b2e85c3fdf5b37f81712
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-19 10:48:20 -0600

    Add food details to view and spec

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -17,6 +17,19 @@ RSpec.describe 'Search foods' do
         it "I should see the total number of items returned by search" do
           expect(page).to have_content('results count: 50')
         end
+
+        it "I should see a list of 10 foods with the query in ingredients" do
+          expect(page).to have_css('section.food', count: 10)
+        end
+
+        it "For each food I should see a list of details" do
+          within 'section.food'.each do
+            expect(page).to have_content("GTIN/UPC code:")
+            expect(page).to have_content("description:")
+            expect(page).to have_content("Brand Owner:")
+            expect(page).to have_content("ingredients:")
+          end
+        end
       end
     end
   end
