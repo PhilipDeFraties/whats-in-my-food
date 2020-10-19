db4282613ca3aead71eeb65d0b388c6faa52db6e
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-19 10:16:15 -0600

    Add test for item quantity from search"

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -4,14 +4,18 @@ RSpec.describe 'Search foods' do
   describe 'As a visitor on the home page' do
     describe "When I fill in the search form with 'sweet potatoes'" do
       describe "And I click 'Search'" do
-        it "I should be on the page '/foods'" do
+        before :each do
           visit '/'
-
           fill_in 'q', with: 'sweet potatoes'
-
           click_button 'Search'
+        end
 
+        it "I should be on the page '/foods'" do
           expect(current_path).to eq('/foods')
+        end
+
+        it "I should see the total number of items returned by search" do
+          expect(page).to have_content('results count: 50')
         end
       end
     end
